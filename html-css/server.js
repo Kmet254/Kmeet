db4eb31ba78594b951b7009430fb05e7b206e138
Kmet254 <juliuskbor74@gmail.com>
2026-09-21 21:27:10 +0500

    Update server.js

--- html-css/server.js
+++ html-css/server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to Kmeet App!');
-});
+// This automatically serves your index.html and other frontend files from the public folder
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
